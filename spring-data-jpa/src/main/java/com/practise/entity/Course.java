263ea052a6dc4eac510470c37d3faa7058846f25
Mahidhar Illuri <reddymahidhar9@gmail.com>
2022-09-22 21:04:59 -0500

    Teacher Test Cases and Course Repository Test Cases

--- spring-data-jpa/src/main/java/com/practise/entity/Course.java
+++ spring-data-jpa/src/main/java/com/practise/entity/Course.java
@@ -6,6 +6,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Builder
@@ -28,4 +30,20 @@ public class Course {
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "teacher_id", referencedColumnName = "teacherId")
     private Teacher teacher;
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "studen_course_map",
+            joinColumns = @JoinColumn(
+                    name = "course_id",
+                    referencedColumnName = "courseId"),
+            inverseJoinColumns = @JoinColumn(
+                    name = "student_id",
+                    referencedColumnName = "studentId")
+    )
+    private List<Student> students;
+
+    public void addStudents(Student student){
+        if(students == null) students = new ArrayList<>();
+        students.add(student);
+    }
 }
